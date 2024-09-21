1844c9890aacc36e83356f0a2f6d2282728232e1
DoDuong1497 <duongdo1497@gmail.com>
2024-09-21 22:56:13 +0700

    first update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import { Dashboard } from "./pages/DashBoard";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+
+      <Dashboard />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Question/Question.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question = () => {
+  return (
+    <>
+      <p>Question page</p>
+    </>
+  );
+};
+
+export default Question;
